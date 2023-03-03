5456d6d8c07ac3d60cd08235452f9b26df10cad2
Justin Mak <jp.mak44@gmail.com>
2023-03-03 15:05:49 -0500

    new commit

---
+++ hamburger.js
@@ -0,0 +1,8 @@
+var hamburgerLinks = document.getElementById("hamburger_menu");
+
+function showMenu(){
+    hamburgerLinks.style.left= "0";
+}
+function hideMenu(){
+    hamburgerLinks.style.left = "-200%"; 
+}
